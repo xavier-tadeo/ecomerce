c3c33adf040084db82757d98dbb1f27997264e68
Tadeo <domenechtadeo@outlook.es>
2022-01-31 21:40:49 +0100

    Create component Register and added first estructuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Register } from "./components/Register/Register";
+
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Register/Register.tsx
@@ -0,0 +1,36 @@
+export const Register = () => {
+  return (
+    <div className="form">
+      <h2 className="form__title">SmartShopping</h2>
+      <form className="form__container">
+        <label htmlFor="firstname" className="form__firstnamelabel">
+          Firts Name
+        </label>
+        <input
+          type="text"
+          id="firstname"
+          placeholder="Your first name"
+          className="form__firstnameinput"
+        />
+        <label htmlFor="lastname" className="form__lastnamelabel">
+          Last Name
+        </label>
+        <input
+          type="text"
+          id="lastname"
+          className="form__lastnameinput"
+          placeholder="Your last name"
+        />
+        <label htmlFor="user" className="form__userlabel">
+          User Name
+        </label>
+        <input
+          type="text"
+          id="user"
+          className="form__userinput"
+          placeholder="Your user name"
+        />
+      </form>
+    </div>
+  );
+};
