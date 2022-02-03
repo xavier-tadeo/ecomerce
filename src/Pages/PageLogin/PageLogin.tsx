e45bea7c237834ed15ced50a67c6e5afd17f009c
Tadeo <domenechtadeo@outlook.es>
2022-02-03 12:33:34 +0100

    Create the PageLogin and added in App

---
+++ src/Pages/PageLogin/PageLogin.tsx
@@ -0,0 +1,11 @@
+import { ImagesRegister } from "../../components/Images/ImagesRegister/ImagesRegister";
+import { Login } from "../../components/Login/Login";
+
+export const PagesLogin = () => {
+  return (
+    <div className="container__page-login">
+      <ImagesRegister />
+      <Login />
+    </div>
+  );
+};
